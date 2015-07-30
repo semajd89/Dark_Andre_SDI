d25d80407fe8ad6fd730e289cf502d4dab0bd25a
Andre <semajd89@gmail.com>
2015-07-30 17:29:27 -0400

    Just entered my lottery numbers
    placed my lottery pick numbers in the function

--- Dark_Andre_Functions_Assignment/js/script.js
+++ Dark_Andre_Functions_Assignment/js/script.js
@@ -7,3 +7,23 @@
 
 //alert("This is a test");
 
+//4 22 27 28 52 35
+
+prompt("Hey would you like the numbers for Florida lottery?");
+
+
+lotteryPick(4,22,27,28,52,35);
+
+function lotteryPick(pick1, pick2, pick3, pick4, pick5, pick6){
+
+alert("The first lottery pick number is " + pick1 + "");
+alert("The second lottery pick number is " + pick2 + "");
+    alert("The third lottery pick number is " + pick3 + "");
+    alert("The fourth lottery pick number is " + pick4 + "");
+    alert("The fifth lottery pick number is " + pick5 + "");
+    alert("The sixth lottery pick number is " +pick6 + "");
+    
+
+
+
+}

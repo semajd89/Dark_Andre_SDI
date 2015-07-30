79ba93b4784a60970cdea6bd0deddf425eddce3b
Andre <semajd89@gmail.com>
2015-07-30 18:33:50 -0400

    Completed Function Assignment
    My completed function assignment with test values at the bottom and
    comments.

--- Dark_Andre_Functions_Assignment/js/script.js
+++ Dark_Andre_Functions_Assignment/js/script.js
@@ -9,30 +9,30 @@
 
 //4 22 27 28 52 35
 
-prompt("Hey would you like the numbers for Florida lottery?");
+prompt("Hey would you like the numbers for Florida lottery?"); //prompt the user the florida pick numbers
 
 
-var flaLottery = lotteryPick(4,22,27,28,52,35);
+var flaLottery = lotteryPick(4,22,27,28,52,35); //function call with the arguments as my pick numbers
 
-function lotteryPick(pick1, pick2, pick3, pick4, pick5, pick6){
+function lotteryPick(pick1, pick2, pick3, pick4, pick5, pick6){ //function setup
 
-alert("The first lottery pick number is " + pick1 + "");
+alert("The first lottery pick number is " + pick1 + ""); //alerts the user the florida pick numbers one by one
 alert("The second lottery pick number is " + pick2 + "");
     alert("The third lottery pick number is " + pick3 + "");
     alert("The fourth lottery pick number is " + pick4 + "");
     alert("The fifth lottery pick number is " + pick5 + "");
     alert("The sixth lottery pick number is " +pick6 + "");
 
-    var lotteryNum = [pick1, pick2, pick3, pick4, pick5, pick6];
+    var lotteryNum = [pick1, pick2, pick3, pick4, pick5, pick6]; // return the value for console.log to print out
     return lotteryNum;
 
 
 
 }
+alert("The Florida lottery numbers is " +flaLottery+".",  "If you picked these numbers then you are the lucky winner. Congratulations!!!" +
+" "); //alerts the user the final florida lottery numbers
 
-
-console.log("The Florida lottery numbers is " +flaLottery+".",  "If you picked these numbers then you are the lucky winner. Congratulations!!!" +
-" ");
+console.log("The Florida lottery numbers is " +flaLottery+ ""); //print out to the console of the final value
 
 
 /*lotteryPick2(55,86)
@@ -41,4 +41,4 @@ function lotteryPick2 (pick1, pick2){
     alert("The first lottery pick number is " +pick1+ "")
 }
 
-This was a test to 
+This was a test to prove the numbers I plug in worked with the function
